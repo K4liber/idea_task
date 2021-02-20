aa1033fb738081a7afb8a8e6aaf4bfbc3856077d
K4liber <janbielecki94@gmail.com>
2021-02-20 13:02:36 +0100

    quicker docker build from ubuntu

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 app="idea"
-docker build -t ${app} .
+docker_file="DockerfileUbuntu"
+if [ "${1}" != "" ] ; then
+  echo 'yolo'
+  docker_file=$1
+fi
+echo 'Building with docker_file =' "${docker_file}"
+docker build -t ${app} . -f "${docker_file}"
 docker stop ${app}
 docker rm ${app}
 docker run -d -p 8050:8050 \
